b71631a2b67aee4683ac362e19e2723249d7b981
Vinicius Almeida <vinicalmeida38@gmail.com>
2021-11-29 01:49:39 -0300

    Login page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,6 +3,7 @@ import { Route, BrowserRouter } from "react-router-dom";
 import About from "../pages/About/About";
 import Guides from "../pages/Guides/Guides";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
 
 const Routes = () => {
   return (
@@ -10,6 +11,7 @@ const Routes = () => {
       <Route component={Home} path="/" exact />
       <Route component={Guides} path="/roteiros" />
       <Route component={About} path="/sobre" />
+      <Route component={Login} path="/admin" />
     </BrowserRouter>
   );
 };
