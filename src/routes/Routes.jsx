373324dd021cfbd1d07af1e552f0267c259c0749
Vinicius Almeida <vinicalmeida38@gmail.com>
2021-11-29 21:39:54 -0300

    Add Edit Guide page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 import About from "../pages/About/About";
+import EditGuide from "../pages/EditGuide/EditGuide";
 import Guides from "../pages/Guides/Guides";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
@@ -14,6 +15,7 @@ const Routes = () => {
       <Route component={About} path="/sobre" />
       <Route component={Login} path="/admin" />
       <Route component={NewGuide} path="/novo-roteiro" />
+      <Route component={EditGuide} path="/editar-roteiro/:id" />
     </BrowserRouter>
   );
 };
